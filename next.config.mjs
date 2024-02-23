be07955b0cd9f6907ba002db1f1d40aefa453902
R Shivaram <85446156+shivaramr@users.noreply.github.com>
2024-02-23 16:36:36 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    domains: ['shivaramr.github.io/'],
+  },
   experimental: {
   appDir: false,
   externalDir: true | {
