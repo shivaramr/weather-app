1cdb8b0567029bcade2a298096ec314ab7514301
shivaramr <85446156+shivaramr@users.noreply.github.com>
2024-02-24 14:54:38 +0530

    run dev issue fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  distDir: "dist",
   images: {
-    // domains: ['shivaramr.github.io'],
     unoptimized: true,
   },
-  staticPageGenerationTimeout: 1000,
 };
 
 export default nextConfig;
